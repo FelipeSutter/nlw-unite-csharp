da3c358226c776b21363c0f2fa413092a26fcc61
FelipeSutter <lipeps61@gmail.com>
2024-04-04 09:46:04 -0300

    feat: Aula 01 Completa

--- PassIn.Api/Controllers/EventsController.cs
+++ PassIn.Api/Controllers/EventsController.cs
@@ -35,5 +35,29 @@ namespace PassIn.Api.Controllers;
 
         }
 
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(ResponseEventJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            try
+            {
+                var useCase = new GetEventByIdUseCase();
+
+                var response = useCase.Execute(id);
+
+                return Ok(response);
+            }
+            catch (PassInException ex)
+            {
+                return NotFound(new ResponseErrorJson(ex.Message));
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorJson("Unknown error"));
+            }
+
+        }
     }
 
